711b78a1f83383bbd6603a21e8ee3c996593cef9
leannewilson <83836254+leannewilson@users.noreply.github.com>
2021-07-16 14:00:04 -0700

    iteration 5 done

--- backend/index.js
+++ backend/index.js
@@ -1,24 +1,37 @@
 //const movies = require ('./bin/seeds');
-const mongoose = require ('mongoose');
-mongoose.connect('mongodb://localhost:27017/movies', {useNewUrlParser: true, useUnifiedTopology: true});
-const express = require('express')
-const app = express()
-const Movie = require("./models/Movie")
-const router = express.Router()
+const mongoose = require("mongoose");
+mongoose.connect(
+  "mongodb+srv://mahim:mahim@cluster0.r3bte.mongodb.net/mymovies?retryWrites=true&w=majority",
+  { useNewUrlParser: true, useUnifiedTopology: true }
+);
+const express = require("express");
+const app = express();
+const Movie = require("./models/Movie");
+const router = express.Router();
 const cors = require("cors");
+const bodyParser = require("body-parser");
+app.use(bodyParser.json());
+
 app.use(cors());
 
-app.get('/listOfMovies', (req, res) => {
-    Movie.find().then((movies) => {
-        res.json(movies)
-    })
-})
+app.get("/listOfMovies", (req, res) => {
+  Movie.find().then((movies) => {
+    res.json(movies);
+  });
+});
 
-app.get('/movieDetails/:dynamicId', (req, res) => {
-    console.log(req.params);
-    Movie.findOne({_id: req.params.dynamicId}).then((movies) => {
-        res.json(movies);
-    })
-})
+app.get("/movieDetails/:dynamicId", (req, res) => {
+  console.log(req.params);
+  Movie.findOne({ _id: req.params.dynamicId }).then((movies) => {
+    res.json(movies);
+  });
+});
 
-app.listen(5000)
+app.post("/newmovie", (req, res) => {
+  console.log("newmovie", req.body);
+  Movie.create(req.body).then((movies) => {
+    res.json(movies);
+  });
+});
+
+app.listen(5000);
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,23 @@
-import logo from './logo.svg';
-import { Switch, Route } from 'react-router-dom';
-import './App.css';
-import Home from './components/Home';
+import logo from "./logo.svg";
+import { Switch, Route } from "react-router-dom";
+import "./App.css";
+import Home from "./components/Home";
 import Movies from "./components/Movies";
 import Details from "./components/Details";
+import NewMovies from "./components/NewMovies";
 
 function App() {
   return (
     <div className="App">
       <Switch>
-        <Route exact path='/' render={(props) => <Home/>}/>
-        <Route exact path='/movies' render={(props) => <Movies/>}/>
-        <Route exact path='/movie/:dynamicId' render={(props) => <Details {...props} />}/>
+        <Route exact path="/" render={(props) => <Home />} />
+        <Route exact path="/movies" render={(props) => <Movies />} />
+        <Route
+          exact
+          path="/movie/:dynamicId"
+          render={(props) => <Details {...props} />}
+        />
+        <Route exact path="/newmovie" render={(props) => <NewMovies />} />
       </Switch>
     </div>
   );
